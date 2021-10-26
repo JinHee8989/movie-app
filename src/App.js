536c39d750ed987a9d9f00540f2c5aace8932093
jinheebyeon <wlsgml8989@gmail.com>
2021-10-26 23:23:01 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
-const foodILike = [{
-  id : 1,
-  name : "Gimbab",
-  image : "https://pixabay.com/ko/photos/%ec%9d%8c%ec%8b%9d%ec%82%ac%ec%a7%84-%ed%95%9c%ec%8b%9d-%ea%b9%80%eb%b0%a5-2610864/",
-  rating : 3
-},
-{
-  id:2,
-  name : "Ramen",
-  image : "https://pixabay.com/ko/photos/%eb%9d%bc%eb%a9%b4-%ec%9d%bc%eb%b3%b8-%ec%9d%8c%ec%8b%9d-%ec%9a%94%eb%a6%ac-2199962/",
-  rating : 4
-}]
-
-function Food({name,picture,rating}){  // =function Food(props.fav){}
-  return <div>
-    <h1>I Like {name}</h1> 
-    <h4>{rating}/5.0</h4>
-    <img src = {picture} alt = {name}/>
-  
-  </div>
-}
-
-Food.propTypes={ //propTypes로 체크할 수 있음
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      <h1>hello!!!</h1>
-
-      {foodILike.map(dish => <Food key={dish.id} name ={dish.name} picture = {dish.image} rating = {dish.rating}/>)}
-      
-    </div>
-  );
+class App extends React.Component {
+  state = { //state는 object임
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => { 
+    console.log("minus"); 
+  };
+  render() {
+    return <div><h1>The number is {this.state.count}.</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>;
+  };
 }
 
 export default App;
