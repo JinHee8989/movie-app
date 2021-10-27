b788a292f46a3c5a95153c3715390d0ffbebaa03
jinheebyeon <wlsgml8989@gmail.com>
2021-10-27 22:50:04 +0900

    setting state

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends React.Component {
   };
   add = () => {
     console.log("add");
+    this.setState(current => ({count : current.count + 1})); //setState를 할때마다 react는 render를 호출한다.
   };
   minus = () => { 
     console.log("minus"); 
+    this.setState(current => ({count : current.count - 1}));
   };
   render() {
     return <div><h1>The number is {this.state.count}.</h1>
