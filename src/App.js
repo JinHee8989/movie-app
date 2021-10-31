934b4b1b0d5569ada54b6998dd059b1331ca5eb3
jinheebyeon <wlsgml8989@gmail.com>
2021-10-31 16:12:51 +0900

    constructor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
+
+  constructor(){
+    console.log("hello");
+  }
+
   state = { //state는 object임
     count: 0
   };
@@ -14,6 +18,7 @@ class App extends React.Component {
     this.setState(current => ({count : current.count - 1}));
   };
   render() {
+    console.log("render");
     return <div><h1>The number is {this.state.count}.</h1>
       <button onClick={this.add}>Add</button>
       <button onClick={this.minus}>Minus</button>
